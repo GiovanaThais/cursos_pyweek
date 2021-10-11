73d19265b5ae6b030c1051d4589ce3ff6e81bea2
Giovana Thais <giovana.carvalho@ma9.com.br>
2021-10-11 20:00:18 -0300

    refactor(admin- app cursos):melhorando admin

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Cursos, Aulas 
 
-admin.site.register(Cursos)
-admin.site.register(Aulas)
+@admin.register(Cursos)
+class CursosAdmin(admin.ModelAdmin):
+    list_display = ('nome','descricao')
+
+@admin.register(Aulas)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome','curso','descricao')
+
