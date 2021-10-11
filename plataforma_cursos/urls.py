00e5aa282d16c6a91cff5e9007bcb43c33f12317
Giovana Thais <giovana.carvalho@ma9.com.br>
2021-10-11 19:10:35 -0300

    feat:add app cursos e funcionalidades da tela

--- plataforma_cursos/urls.py
+++ plataforma_cursos/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
+    path('home/', include('cursos.urls')),
 
 ]
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -5,12 +5,18 @@ from django.shortcuts import redirect
 import hashlib
 
 def cadastro(request):
+    if request.session.get('usuario'):
+        return redirect('/home/')
     status = request.GET.get('status')
     return render(request, 'cadastro.html', {'status': status})
 
 def login(request):
     status = request.GET.get('status')
     return render(request, 'login.html', {'status': status})
+
+def sair(request):
+    request.session.flush() #remove tudo do usuario no sistema
+    return redirect('/auth/login/')
 
 def valida_cadastro(request):
     nome = request.POST.get('nome')
